93e3f3ca2c00a384944e0fca8a8ab794ac5118d4
user_name <email_id>
2023-10-12 12:32:07 +0300

    complete assignment

--- index.js
+++ index.js
@@ -5,6 +5,32 @@ function hasTargetSum(array, target) {
 /* 
   Write the Big O time complexity of your function here
 */
+function hasTargetSum(arr, target) {
+  // Initialize a Set to store numbers seen so far
+  const numSet = new Set();
+
+  // Iterate through the array
+  for (let i = 0; i < arr.length; i++) {
+    // Calculate the complement needed to reach the target
+    const complement = target - arr[i];
+
+    // If the complement exists in the Set, return true
+    if (numSet.has(complement)) {
+      return true;
+    }
+
+    // Add the current element to the Set
+    numSet.add(arr[i]);
+  }
+
+  // If no pair is found, return false
+  return false;
+}
+
+// Test cases
+console.log(hasTargetSum([3, 8, 12, 4, 11, 7], 10)); // Should return true
+console.log(hasTargetSum([22, 19, 4, 6, 30], 25)); // Should return true
+console.log(hasTargetSum([1, 2, 5], 4)); // Should return false
 
 /* 
   Add your pseudocode here
